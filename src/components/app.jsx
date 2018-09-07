ae4843bb5040ff964dc1cc67b520d324107961f2
Alex Devero <deveroalex@gmail.com>
2018-09-07 11:36:43 +0200

    Abstract settings to stateless component

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import ScreenSettings from './screenSettings'
+
 import '../styles/styles.css'
 
 class App extends React.Component {
@@ -11,7 +13,8 @@ class App extends React.Component {
       durationExercise: 20,
       durationRest: 10,
       durationWarmup: 0,
-      durationCooldown: 0
+      durationCooldown: 0,
+      isReady: false
     }
 
     this.handleChange = this.handleChange.bind(this)
@@ -36,93 +39,7 @@ class App extends React.Component {
       <div className="app-wrapper">
         <h1>Tabata timer</h1>
 
-        <div>
-          <fieldset>
-            <label htmlFor="numOfSets">Number of sets</label>
-
-            <input type="number" name="numOfSets" id="numOfSets" value={this.state.numOfSets} onChange={this.handleChange} />
-          </fieldset>
-
-          <fieldset>
-            <label htmlFor="durationExercise">Exercise</label>
-
-            <input type="number" name="durationExercise" id="durationExercise" value={this.state.durationExercise} onChange={this.handleChange} />
-
-            <select name="colorExercise" id="colorExercise" onChange={this.handleChange}>
-              <option value="gray">Gray</option>
-              <option value="red">Red</option>
-              <option value="orange">Orange</option>
-              <option value="yellow">Yellow</option>
-              <option value="green">Green</option>
-              <option value="teal">Teal</option>
-              <option value="blue">Blue</option>
-              <option value="purple">Purple</option>
-              <option value="magenta">Magenta</option>
-              <option value="deep-purple">Deep purple</option>
-            </select>
-          </fieldset>
-
-          <fieldset>
-            <label htmlFor="durationRest">Rest</label>
-
-            <input type="number" name="durationRest" id="durationRest" value={this.state.durationRest} onChange={this.handleChange} />
-
-            <select name="colorRest" id="colorRest" onChange={this.handleChange}>
-              <option value="gray">Gray</option>
-              <option value="red">Red</option>
-              <option value="orange">Orange</option>
-              <option value="yellow">Yellow</option>
-              <option value="green">Green</option>
-              <option value="teal">Teal</option>
-              <option value="blue">Blue</option>
-              <option value="purple">Purple</option>
-              <option value="magenta">Magenta</option>
-              <option value="deep-purple">Deep purple</option>
-            </select>
-          </fieldset>
-
-          <fieldset>
-            <label htmlFor="durationWarmup">Warmup</label>
-
-            <input type="number" name="durationWarmup" id="durationWarmup" value={this.state.durationWarmup} onChange={this.handleChange} />
-
-            <select name="colorWarmup" id="colorWarmup" onChange={this.handleChange}>
-              <option value="gray">Gray</option>
-              <option value="red">Red</option>
-              <option value="orange">Orange</option>
-              <option value="yellow">Yellow</option>
-              <option value="green">Green</option>
-              <option value="teal">Teal</option>
-              <option value="blue">Blue</option>
-              <option value="purple">Purple</option>
-              <option value="magenta">Magenta</option>
-              <option value="deep-purple">Deep purple</option>
-            </select>
-          </fieldset>
-
-          <fieldset>
-            <label htmlFor="durationCooldown">Cooldown</label>
-
-            <input type="number" name="durationCooldown" id="durationCooldown" value={this.state.durationCooldown} onChange={this.handleChange} />
-
-            <select name="colorCooldown" id="colorCooldown" onChange={this.handleChange}>
-              <option value="gray">Gray</option>
-              <option value="red">Red</option>
-              <option value="orange">Orange</option>
-              <option value="yellow">Yellow</option>
-              <option value="green">Green</option>
-              <option value="teal">Teal</option>
-              <option value="blue">Blue</option>
-              <option value="purple">Purple</option>
-              <option value="magenta">Magenta</option>
-              <option value="deep-purple">Deep purple</option>
-            </select>
-          </fieldset>
-
-          <fieldset>
-            <button onClick={this.handleClick}>Create timer</button>
-          </fieldset>
-        </div>
+        {!this.state.isReady && <ScreenSettings state={this.state} handleChange={this.handleChange} handleClick={this.handleClick} />}
       </div>
     )
   }
