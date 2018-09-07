b8409dfad07a8fc25842214c9c79bc0b7820d965
Alex Devero <deveroalex@gmail.com>
2018-09-07 11:07:50 +0200

    Add input change handler

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import '../styles/styles.css'
+
 class App extends React.Component {
   constructor(props) {
     super(props)
@@ -11,26 +13,42 @@ class App extends React.Component {
       durationWarmup: 0,
       durationCooldown: 0
     }
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick() {
+    console.log('clicked')
+  }
+
+  handleChange(e) {
+    let elId = e.target.id
+    let value = parseInt(e.target.value)
+
+    this.setState({
+      [elId]: value
+    })
   }
 
   render() {
     return (
-      <React.Fragment>
+      <div className="app-wrapper">
         <h1>Tabata timer</h1>
 
         <div>
           <fieldset>
             <label htmlFor="numOfSets">Number of sets</label>
 
-            <input type="number" name="numOfSets" id="numOfSets" defaultValue={this.state.numOfSets} />
+            <input type="number" name="numOfSets" id="numOfSets" value={this.state.numOfSets} onChange={this.handleChange} />
           </fieldset>
 
           <fieldset>
-            <label htmlFor="exercise">Exercise</label>
+            <label htmlFor="durationExercise">Exercise</label>
 
-            <input type="number" name="exercise" id="exercise" defaultValue={this.state.durationExercise} />
+            <input type="number" name="durationExercise" id="durationExercise" value={this.state.durationExercise} onChange={this.handleChange} />
 
-            <select name="colorExercise" id="colorExercise">
+            <select name="colorExercise" id="colorExercise" onChange={this.handleChange}>
               <option value="gray">Gray</option>
               <option value="red">Red</option>
               <option value="orange">Orange</option>
@@ -40,16 +58,16 @@ class App extends React.Component {
               <option value="blue">Blue</option>
               <option value="purple">Purple</option>
               <option value="magenta">Magenta</option>
-              <option value=""></option>
+              <option value="deep-purple">Deep purple</option>
             </select>
           </fieldset>
 
           <fieldset>
-            <label htmlFor="rest">Rest</label>
+            <label htmlFor="durationRest">Rest</label>
 
-            <input type="number" name="rest" id="rest" defaultValue={this.state.durationRest} />
+            <input type="number" name="durationRest" id="durationRest" value={this.state.durationRest} onChange={this.handleChange} />
 
-            <select name="colorRest" id="colorRest">
+            <select name="colorRest" id="colorRest" onChange={this.handleChange}>
               <option value="gray">Gray</option>
               <option value="red">Red</option>
               <option value="orange">Orange</option>
@@ -59,16 +77,16 @@ class App extends React.Component {
               <option value="blue">Blue</option>
               <option value="purple">Purple</option>
               <option value="magenta">Magenta</option>
-              <option value=""></option>
+              <option value="deep-purple">Deep purple</option>
             </select>
           </fieldset>
 
           <fieldset>
-            <label htmlFor="warmup">Warmup</label>
+            <label htmlFor="durationWarmup">Warmup</label>
 
-            <input type="number" name="warmup" id="warmup" defaultValue={this.state.durationWarmup} />
+            <input type="number" name="durationWarmup" id="durationWarmup" value={this.state.durationWarmup} onChange={this.handleChange} />
 
-            <select name="colorWarmup" id="colorWarmup">
+            <select name="colorWarmup" id="colorWarmup" onChange={this.handleChange}>
               <option value="gray">Gray</option>
               <option value="red">Red</option>
               <option value="orange">Orange</option>
@@ -78,16 +96,16 @@ class App extends React.Component {
               <option value="blue">Blue</option>
               <option value="purple">Purple</option>
               <option value="magenta">Magenta</option>
-              <option value=""></option>
+              <option value="deep-purple">Deep purple</option>
             </select>
           </fieldset>
 
           <fieldset>
-            <label htmlFor="cooldown">Cooldown</label>
+            <label htmlFor="durationCooldown">Cooldown</label>
 
-            <input type="number" name="cooldown" id="cooldown" defaultValue={this.state.durationCooldown} />
+            <input type="number" name="durationCooldown" id="durationCooldown" value={this.state.durationCooldown} onChange={this.handleChange} />
 
-            <select name="colorCooldown" id="colorCooldown">
+            <select name="colorCooldown" id="colorCooldown" onChange={this.handleChange}>
               <option value="gray">Gray</option>
               <option value="red">Red</option>
               <option value="orange">Orange</option>
@@ -97,11 +115,15 @@ class App extends React.Component {
               <option value="blue">Blue</option>
               <option value="purple">Purple</option>
               <option value="magenta">Magenta</option>
-              <option value=""></option>
+              <option value="deep-purple">Deep purple</option>
             </select>
+          </fieldset>
+
+          <fieldset>
+            <button onClick={this.handleClick}>Create timer</button>
           </fieldset>
         </div>
-      </React.Fragment>
+      </div>
     )
   }
 }
